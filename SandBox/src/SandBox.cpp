fb20016e18756b04ab9bc86032ee35d8f37cce4a
AALXX <s3rb@DESKTOP-2D6QHO6.localdomain>
2022-11-27 23:11:53 +0200

    added transform

--- SandBox/src/SandBox.cpp
+++ SandBox/src/SandBox.cpp
@@ -1,4 +1,5 @@
 #include <PlutoniumGameEngine.h>
+#include <glm/gtc/matrix_transform.hpp>
 
 #include "imgui/imgui.h"
 
@@ -31,10 +32,10 @@ public:
 		m_SquareVA.reset(PGE::VertexArray::Create());
 
 		float squareVertices[3 * 4] = {
-			-0.75f, -0.75f, 0.0f,
-			 0.75f, -0.75f, 0.0f,
-			 0.75f,  0.75f, 0.0f,
-			-0.75f,  0.75f, 0.0f
+			-0.5f, -0.5f, 0.0f,
+			 0.5f, -0.5f, 0.0f,
+			 0.5f,  0.5f, 0.0f,
+			-0.5f,  0.5f, 0.0f
 		};
 
 		std::shared_ptr<PGE::VertexBuffer> squareVB;
@@ -56,6 +57,7 @@ public:
 			layout(location = 1) in vec4 a_Color;
 
 			uniform mat4 u_ViewProjection;
+			uniform mat4 u_ModelMatrix;
 
 			out vec3 v_Position;
 			out vec4 v_Color;
@@ -63,7 +65,7 @@ public:
 			{
 				v_Position = a_Position;
 				v_Color = a_Color;
-				gl_Position = u_ViewProjection * vec4(a_Position, 1.0);	
+				gl_Position = u_ViewProjection * u_ModelMatrix * vec4(a_Position, 1.0);	
 			}
 		)";
 
@@ -88,13 +90,15 @@ public:
 			layout(location = 0) in vec3 a_Position;
 
 			uniform mat4 u_ViewProjection;
+			uniform mat4 u_ModelMatrix;
+
 
 			out vec3 v_Position;
 
 			void main()
 			{
 				v_Position = a_Position;
-				gl_Position = u_ViewProjection * vec4(a_Position, 1.0);	
+				gl_Position = u_ViewProjection * u_ModelMatrix * vec4(a_Position, 1.0);	
 			}
 		)";
 
@@ -114,20 +118,21 @@ public:
 
 	void OnUpdate(PGE::Timestep ts) override {
 
-		if (PGE::Input::IsKeyPressed(PGE_KEY_LEFT))
+		if (PGE::Input::IsKeyPressed(PGE_KEY_A))
 			m_CameraPosition.x -= m_CameraMoveSpeed * ts;
-		else if (PGE::Input::IsKeyPressed(PGE_KEY_RIGHT))
+		else if (PGE::Input::IsKeyPressed(PGE_KEY_D))
 			m_CameraPosition.x += m_CameraMoveSpeed * ts;
 
-		if (PGE::Input::IsKeyPressed(PGE_KEY_UP))
+		if (PGE::Input::IsKeyPressed(PGE_KEY_W))
 			m_CameraPosition.y += m_CameraMoveSpeed * ts;
-		else if (PGE::Input::IsKeyPressed(PGE_KEY_DOWN))
+		else if (PGE::Input::IsKeyPressed(PGE_KEY_S))
 			m_CameraPosition.y -= m_CameraMoveSpeed * ts;
 
-		if (PGE::Input::IsKeyPressed(PGE_KEY_A))
+		if (PGE::Input::IsKeyPressed(PGE_KEY_Q))
 			m_CameraRotation += m_CameraRotationSpeed * ts;
-		if (PGE::Input::IsKeyPressed(PGE_KEY_D))
+		if (PGE::Input::IsKeyPressed(PGE_KEY_E))
 			m_CameraRotation -= m_CameraRotationSpeed * ts;
+
 
 		PGE::RenderCommand::SetClearColor({ 0.1f, 0.1f, 0.1f, 1.0f });
 		PGE::RenderCommand::Clear();
@@ -135,12 +140,22 @@ public:
 		m_Camera.SetPosition(m_CameraPosition);
 		m_Camera.SetRotation(m_CameraRotation);
 
-
 		PGE::GraphicsEngine::BeginScene(m_Camera);
 
-		PGE::GraphicsEngine::Submit(m_BlueShader, m_SquareVA);
+		glm::mat4 scale = glm::scale(glm::mat4(1.0f), glm::vec3(0.1f));
+
+		for (int y = 0; y < 20; y++)
+		{
+			for (int x = 0; x < 20; x++)
+			{
+				glm::vec3 pos(x * 0.11f, y * 0.11f, 0.0f);
+				glm::mat4 transform = glm::translate(glm::mat4(1.0f), pos) * scale;
+				PGE::GraphicsEngine::Submit(m_BlueShader, m_SquareVA, transform);
+			}
+		}
 
 		PGE::GraphicsEngine::Submit(m_Shader, m_VertexArray);
+
 
 		PGE::GraphicsEngine::EndScene();
 	}
@@ -153,6 +168,7 @@ public:
 	virtual void OnImGuiRender() override {
 
 	}
+
 private:
 
 	std::shared_ptr<PGE::Shader> m_Shader;
@@ -168,6 +184,7 @@ private:
 
 	float m_CameraRotation = 0.0f;
 	float m_CameraRotationSpeed = 180.0f;
+
 };
 
 class Sandbox : public PGE::Application
